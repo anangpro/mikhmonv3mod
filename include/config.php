1bfce5138eefc72e81c326359cb43961805833d2
anangpro <126405279+anangpro@users.noreply.github.com>
2023-02-28 15:24:54 +0800

    Add files via upload

---
+++ include/config.php
@@ -0,0 +1,6 @@
+<?php 
+if(substr($_SERVER["REQUEST_URI"], -10) == "config.php"){header("Location:./");}; 
+$data['mikhmon'] = array ('1'=>'mikhmon<|<anang','mikhmon>|>qKOh');
+
+$data['KOSTNET'] = array ('1'=>'KOSTNET!remote.vpnmurahjogja.my.id:59215','KOSTNET@|@voucher','KOSTNET#|#qKOh','KOSTNET%kostnet','KOSTNET^kost.net','KOSTNET&Rp','KOSTNET*10','KOSTNET(1','KOSTNET)','KOSTNET=10','KOSTNET@!@enable');
+$data['Kediri'] = array ('1'=>'Kediri!remote.vpnmurahjogja.my.id:37103','Kediri@|@anang','Kediri#|#qKOh','Kediri%kost.net','Kediri^Kost.net','Kediri&Rp','Kediri*10','Kediri(1','Kediri)','Kediri=10','Kediri@!@enable');
